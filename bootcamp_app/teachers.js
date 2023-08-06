52e9263903b4a9766862bae5de08078ba359f082
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-08-05 23:01:41 -0700

    make teacher queries using teacher.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const args = process.argv.splice(2);
+
+pool
+  .query(
+    `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${args[0]}'
+ORDER BY teacher;
+`
+  )
+  .then((res) => {
+    console.log(res.rows);
+    res.rows.forEach((user) => console.log(`${user.cohort}: ${user.teacher} `));
+  })
+  .catch((err) => console.error('query error', err.stack));
